f04e12c7be75ae9e9dee6b8504b509c1cd0c43f1
rahulgupta20nov <rahul.g@smartlybuilt.com>
2017-04-10 21:18:42 +0530

    problem in angular-material

---
+++ app.js
@@ -0,0 +1,21 @@
+define(['ui-router'], function() {
+    var app = angular.module('myApp', ['ui.router', 'ngMaterial']);
+    app.controller('LoginCtrl', function() {
+
+    });
+
+    app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider
+            .otherwise('login')
+            .when('/', 'login');
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'login.html',
+                controller: 'LoginCtrl'
+            });
+    }]);
+
+    app.controller('AppController', function($scope) {
+    });
+});
